4723946aa9d2b16dcff561d203f3f499bfa6be56
Trystan Lea <trystan.lea@gmail.com>
2025-03-26 18:36:16 +0000

    givenergy ems basic poll

---
+++ ems/fetch_data.py
@@ -0,0 +1,61 @@
+import time
+import requests
+import json
+
+import configparser
+
+# get script directory
+script_dir = sys.path[0]
+
+# load config.ini
+config = configparser.ConfigParser()
+config.read(script_dir+'/config.ini')
+
+emoncms_apikey = config['emoncms_apikey']
+givenergy_ems_ip = config['givenergy_ems_ip']
+
+# Define URLs
+source_url = f"https://{givenergy_ems_ip}/action/getSysSummaryInfoAction?name=User"
+target_url = f"https://emoncms.org/input/post?node=givenergy&apikey={emoncms_apikey}"
+
+# Disable SSL warnings (useful for local HTTPS requests with self-signed certs)
+requests.packages.urllib3.disable_warnings()
+
+def fetch_data():
+    try:
+
+        # add random=0.8519328280130646 to the end of the URL to prevent caching
+        source_url = source_url + "&random=" + str(random.random())
+        print ("source_url: ", source_url)
+        
+        response = requests.get(source_url, verify=False, timeout=5)
+        response.raise_for_status()
+        data = response.json()
+        if "msg" in data:
+            return data["msg"]
+        else:
+            print("Unexpected response format:", data)
+            return None
+    except requests.RequestException as e:
+        print("Error fetching data:", e)
+        return None
+
+def send_to_emoncms(data):
+    try:
+        json_payload = json.dumps(data)
+        full_url = f"{target_url}&fulljson={json_payload}"
+        response = requests.get(full_url)
+        response.raise_for_status()
+        print("Data sent successfully:", response.text)
+    except requests.RequestException as e:
+        print("Error sending data:", e)
+
+def main():
+    while True:
+        data = fetch_data()
+        if data:
+            send_to_emoncms(data)
+        time.sleep(10)
+
+if __name__ == "__main__":
+    main()
